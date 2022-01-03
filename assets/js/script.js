4c559e7180f21fff39e16981348bb002ff06debd
hjohnsick <hjohnsick@yahoo.com>
2022-01-02 19:17:28 -0500

    added functionality to check time

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ currentDayEl.textContent = moment().format("dddd, MMMM Do YYYY");
 
 var containerDiv = document.querySelector(".container");
 var textarea = document.querySelector("textarea");
-var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
+var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM"];
+var militaryTimes = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
 // Store the current time
-var currentTime = moment().format("hA");
+var currentTime = moment().format("H");
 
 // Create time blocks
 var createTimeBlocks = function() {
@@ -28,8 +28,8 @@ var createTimeBlocks = function() {
     var buttonDiv = $('<div>').addClass("col");
     var button = $('<button class="saveBtn"><i class="fas fa-save"></i></button>');
     $(buttonDiv).append(button);
-    // $(row).append(timeTD, textInputTD, buttonTD);
     $(divRow).append(timeDiv, textInputDiv, buttonDiv);
+    checkTime(i, textArea);
  }
 }
 
@@ -42,14 +42,30 @@ var saveTasks = function() {
     localStorage.setItem("tasks", );
 }
 
-var checkTime = function(textArea) {
+var checkTime = function(i, textArea) {
+    // remove any old classes from element
+    $(textArea).removeClass("past present future");
 
+    // apply new class if task is future, present, or past
+        if (currentTime > militaryTimes[i]) {
+            $(textArea).addClass("past");
+        }
+        if (currentTime === militaryTimes[i]) {
+            $(textArea).addClass("present");
+            
+        }
+        if (currentTime < militaryTimes[i]) {
+            $(textArea).addClass("future");
+        }
+   
+        console.log(`This is the current time:${currentTime}`);
+        console.log(militaryTimes[i]);
 }
 
 loadTasks();
 
 setInterval(function() {
     $("textarea").each(function(index, el) {
-        checkTime(el);
+        checkTime(index, el);
     });
-} (1000 * 60) * 60);
+}, 3600000);
